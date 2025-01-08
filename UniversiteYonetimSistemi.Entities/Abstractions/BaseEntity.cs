50ee2bcb94ac36caf420bcd0f052315f8dc44f31
Metehan Atalay <metehanyt2004@gmail.com>
2025-01-08 20:53:09 +0300

    Add project files.

---
+++ UniversiteYonetimSistemi.Entities/Abstractions/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversiteYonetimSistemi.Entities.Abstractions
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Guid ID { get; set; }
+    }
+}
